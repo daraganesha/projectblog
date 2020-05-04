584af5023ad5a67771b762afe826d34996085002
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-05-04 12:15:25 +0200

    tralala

--- src/Blog/HomePage.js
+++ src/Blog/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class HomePage extends React.Component{
     render(){
         return(
-            <div>BLABLU</div>
+            <div>tralalala</div>
         );
     }
 }
